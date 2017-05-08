8f3c39881eb175cbf1a0e0595f3c5e28e298ca0d
Scott Williams <scott@swilliams.me>
2017-05-08 13:16:09 -0700

    Fills out voice

--- voice.sh
+++ voice.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# fetch the file
+# Fetch a file
+curl -s -O https://raw.githubusercontent.com/swilliams/voicetime/master/cupcake.txt
+CUPCAKE=`cat cupcake.txt`
 
+# Repeat this every few seconds
+dostuff () {
+  # repeat randomly between 2 minutes and 10
+  SLEEP_SECS=`jot -r 1 120 600`
 
+  sleep $SLEEP_SECS
+  say $CUPCAKE
+  dostuff
+}
 
-SLEEP_SECS=`jot -r 1 1 5`
-
-sleep $SLEEP_SECS
-say oh hi
-
+dostuff
